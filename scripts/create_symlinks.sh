40656d4f13be2e36a7356d8d38caaa599e6122da
James Santangelo <james.santangelo37@gmail.com>
2021-11-11 10:25:53 -0500

    Adding symlink to script

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -2,3 +2,4 @@ ln -sf ~/github-repos/dotfiles/bashrc ~/.bashrc
 ln -sf ~/github-repos/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/github-repos/dotfiles/vimrc ~/.vimrc
 ln -sf ~/github-repos/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/github-repos/dotfiles/config.py ~/.config/ptpython/config.py
