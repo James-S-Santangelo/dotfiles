3ab11efe0c2dc1ad40111b8f4310234419beb7bb
James Santangelo <james.santangelo37@gmail.com>
2023-05-17 20:43:30 -0700

    Fixing undefined global vim

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -11,6 +11,17 @@ lsp.ensure_installed({
   'marksman'
 })
 
+-- Fix Undefined global 'vim'
+lsp.configure('lua_ls', { --
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
